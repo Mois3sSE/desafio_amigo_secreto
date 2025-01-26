f96753c9321404ec349e41b7702442815ff33033
Moises Sandoval <moises.sandoval.rivero@gmail.com>
2025-01-26 13:12:30 -0600

    Desarrollando el app

--- app.js
+++ app.js
@@ -1,1 +1,25 @@
 // El principal objetivo de este desafío es fortalecer tus habilidades en lógica de programación. Aquí deberás desarrollar la lógica para resolver el problema.
+
+let amigos = []; 
+
+function agregarAmigo(){
+let nombre = document.getElementById('amigo').value;
+
+if(nombre === ""){
+    alert('Por favor, inserte un nombre'); 
+} else {
+    amigos.push(nombre); 
+    console.log(amigos); 
+    limpiarCampoEntrada(); 
+}
+return; 
+}
+
+function actualizarLista (){
+    
+    return; 
+}
+
+function limpiarCampoEntrada(){
+    document.getElementById('amigo').value = ''; 
+}
