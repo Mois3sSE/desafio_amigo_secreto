3bd8af86d4b593b11d7c13f5d0d7a816ee56fb61
Moises Sandoval <moises.sandoval.rivero@gmail.com>
2025-01-26 13:56:15 -0600

    Finalizacion del proyecto

--- app.js
+++ app.js
@@ -10,16 +10,43 @@ if(nombre === ""){
 } else {
     amigos.push(nombre); 
     console.log(amigos); 
+    actualizarLista();
+
     limpiarCampoEntrada(); 
 }
 return; 
 }
 
 function actualizarLista (){
+    let lista = document.getElementById('listaAmigos'); 
     
+
+    lista.innerHTML = ""; 
+
+    for(var i = 0; i < amigos.length; i++){
+    let li = document.createElement('li');  
+    li.textContent = amigos[i]; 
+    lista.appendChild (li); 
+    }
+
     return; 
 }
+
+function sortearAmigo(){
+if(amigos.length === 0 ){
+    alert("Tu arreglo esta vacio"); 
+} else {
+    let indice = Math.floor((Math.random()*amigos.length));
+    console.log(`El valor de los amigos es : ${amigos.length}`) 
+    console.log(indice); 
+    let amigoSecreto = amigos[indice]; 
+    document.getElementById('resultado').innerHTML = amigoSecreto; 
+
+}
+}
+
 
 function limpiarCampoEntrada(){
     document.getElementById('amigo').value = ''; 
 }
+
